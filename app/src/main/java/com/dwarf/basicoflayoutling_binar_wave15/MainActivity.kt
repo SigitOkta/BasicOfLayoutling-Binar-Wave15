9ac4c9156866b1ff5882a41817aba7f4ff8b2e3f
SigitOkta <oktasigit@gmail.com>
2022-09-14 22:18:07 +0700

    add refactor to constraint + set click

--- app/src/main/java/com/dwarf/basicoflayoutling_binar_wave15/MainActivity.kt
+++ app/src/main/java/com/dwarf/basicoflayoutling_binar_wave15/MainActivity.kt
@@ -2,10 +2,33 @@ package com.dwarf.basicoflayoutling_binar_wave15
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.FrameLayout
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    companion object{
+        private val TAG = MainActivity::class.java.simpleName
+    }
+
+    private val flButtonRight : FrameLayout by lazy {
+        findViewById(R.id.fl_button_right)
+    }
+    private val flButtonLeft : FrameLayout by lazy {
+        findViewById(R.id.fl_button_left)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_linear)
+        flButtonLeft.setOnClickListener {
+            Log.d(TAG,"OnCreate: Button Left Clicked")
+            Toast.makeText(this, "Button Left Clicked", Toast.LENGTH_SHORT).show()
+        }
+        flButtonRight.setOnClickListener {
+            Log.d(TAG,"OnCreate: Button Right Clicked")
+            Toast.makeText(this, "Button Right Clicked", Toast.LENGTH_SHORT).show()
+        }
+
+
     }
 }
